5d711f8cd0f51538a5a5432b9940ed24e3f625f2
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-06-12 19:26:17 -0500

    5-assertion.dart

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,14 +1,5 @@
 void main(List<String> args) {
-  if (args.isEmpty) {
-    print('Please provide a score.');
-    return;
-  }
-
-  try {
-    int nb = int.parse(args[0]);
-    assert(nb >= 80, 'The score must be bigger or equal to 80');
-    print('You Passed');
-  } catch (e) {
-    print('Error: ${e.toString()}');
-  }
+  int nb = int.parse(args[0]);
+  assert(nb >= 80, 'The score must be bigger or equal to 80');
+  print('You Passed');
 }
