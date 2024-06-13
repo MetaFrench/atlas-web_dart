4d99ff480162c8bfb4fc09110292354e2da6476d
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-06-12 19:30:56 -0500

    6-positive_or_negative.dart

--- 0x00-hello-dart/6-positive_or_negative.dart
+++ 0x00-hello-dart/6-positive_or_negative.dart
@@ -1,5 +1,10 @@
 void main(List<String> args) {
   int givenNum = int.parse(args[0]);
-  assert(givenNum >= 80, 'The score must be bigger or equal to 80');
-  print('You Passed');
+  if(givenNum > 0) {
+    print('$givenNum is positive');
+  } else if(givenNum < 0) {
+    print('$givenNum is negative');
+  } else {
+    print('$givenNum is zero');
+  }
 }
