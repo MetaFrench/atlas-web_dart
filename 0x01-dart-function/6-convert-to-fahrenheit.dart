1598e397621159876eaca33203ed6f0e778dc26b
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-06-18 16:45:47 -0500

    6-convert-to-fahrenheit.dart

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,1 +1,6 @@
-List<double> convertToF(List<double> temperaturesInC)
+List<double> convertToF(List<double> temperaturesInC) {
+  return temperaturesInC.map((temp) {
+    double tempInF = (temp * 9/5) + 32;
+    return tempInF.toStringAsFixed(2);
+  }).map(double.parse).toList();
+}
