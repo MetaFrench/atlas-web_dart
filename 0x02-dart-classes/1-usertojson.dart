753881ec21abaa717728036d37d77618bf8d6575
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-06-22 18:41:56 -0500

    1-usertojson.dart

---
+++ 0x02-dart-classes/1-usertojson.dart
@@ -0,0 +1,13 @@
+class User {
+  final String name;
+  final int age;
+  final double height;
+
+  User({required this.name, required this.age, required this.height});
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'age': age,
+        'height': height,
+      };
+}
